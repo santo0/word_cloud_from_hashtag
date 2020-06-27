8576a7364403a4f8f4b10e828c680746a2e0b65e
xavi_nadal <xavinadal14@gmail.com>
2020-06-27 21:40:58 +0200

    flask page

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
