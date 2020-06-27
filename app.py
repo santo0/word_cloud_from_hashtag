d4bc3686deda498d30316b27d2796fd4334b3d0e
xavi_nadal <xavinadal14@gmail.com>
2020-06-28 01:10:14 +0200

    image shown

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
+import os
 import main
 
-app = Flask(__name__)
+IMAGES_FOLDER = os.path.join('static', 'temporary_files')
 
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
 
 @app.route('/', methods=['POST', 'GET'])
 
@@ -11,9 +15,9 @@ def index():
     if request.method == 'POST':
         hashtag_name = request.form['hashtag']
         main.run(hashtag_name)
-        return render_template("search.html")
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'fig.png')
+        return render_template("search.html", image = full_filename)
         
-
     else:
         return render_template("index.html")
 if __name__ == "__main__":
